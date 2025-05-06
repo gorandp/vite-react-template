ac34abbd8beed0aeb4ee432aec53c76a09cc1400
Goran Prpic <gorandp@outlook.com>
2025-05-06 13:15:58 -0300

    fixes

--- src/react-app/routes/__root.tsx
+++ src/react-app/routes/__root.tsx
@@ -1,22 +1,28 @@
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 import "./styles.css"
+import logoTehuelches from "../assets/logo_tehuelches_transparent_background.png";
 
 export const Route = createRootRoute({
     component: () => (
         <>
-            <div className="p-2 flex gap-2">
-                <Link to="/" className="[&.active]:font-bold">
-                    Home
-                </Link>{' '}
-                <Link to="/sales" className="[&.active]:font-bold">
-                    Ventas
-                </Link>{' '}
-                <Link to="/drinks" className="[&.active]:font-bold">
-                    Bebidas
-                </Link>
-            </div>
-            <hr />
+            <header>
+                <div className="left-header">
+                    <img className="logo" src={logoTehuelches} alt="Logo" />
+                    <h1>Tehuelches ventas</h1>
+                </div>
+                <div className="right-header">
+                    <Link to="/" className="nav-btn [&.active]:font-bold">
+                        Inicio
+                    </Link>{' '}
+                    <Link to="/sales" className="nav-btn [&.active]:font-bold">
+                        Ventas
+                    </Link>{' '}
+                    <Link to="/drinks" className="nav-btn [&.active]:font-bold">
+                        Bebidas
+                    </Link>
+                </div>
+            </header>
             <Outlet />
             <TanStackRouterDevtools />
         </>
--- src/react-app/routes/drinks.tsx
+++ src/react-app/routes/drinks.tsx
@@ -1,9 +1,87 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { useState } from "react";
+import { createFileRoute } from '@tanstack/react-router';
+
+import Title from "../components/Title";
+import FormAddDrink from "../components/FormAddDrink";
+import TableDrinks from "../components/TableDrinks";
 
 export const Route = createFileRoute('/drinks')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/drinks"!</div>
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("unknown");
+  //   const [title, setTitle] = useState("Tehuelches SN - Ventas");
+  const title = "Tehuelches SN - Ventas";
+
+  return (
+    <>
+      <head>
+        <Title content={title} />
+      </head>
+      <div className="main">
+
+        <FormAddDrink />
+        <TableDrinks />
+
+
+        <div className="card">
+          <button
+            onClick={() => setCount((count) => count + 1)}
+            aria-label="increment"
+          >
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <div className="card">
+          <button
+            onClick={() => {
+              fetch("/api/drinks/initTable", {
+                method: "POST"
+              })
+                .then((res) => res.json() as Promise<{ name: string }>)
+                .then((data) => setName(data.name));
+            }}
+            aria-label="get name"
+          >
+            Create table drinks {name}
+          </button>
+          <button
+            onClick={() => {
+              fetch("/api/users", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ name: "gogos", password: "1234", roles: ["salesman"] })
+              })
+                .then((res) => res.json() as Promise<{ name: string }>)
+                .then((data) => setName(data.name));
+            }}
+            aria-label="get name"
+          >
+            Add user {name}
+          </button>
+          <button
+            onClick={() => {
+              fetch("/api/drinks")
+                .then((res) => res.json() as Promise<{ name: string }>)
+                .then((data) => setName(data.name));
+            }}
+            aria-label="get name"
+          >
+            Get drinks {name}
+          </button>
+          <p>
+            Edit <code>worker/index.ts</code> to change the name
+          </p>
+        </div>
+        <p className="read-the-docs">Click on the logos to learn more</p>
+      </div>
+    </>
+  )
 }
