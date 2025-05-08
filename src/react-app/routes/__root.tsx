a3b3a57b27c1c0c4aee88eec2d98dfd2e977d510
Goran Prpic <gorandp@outlook.com>
2025-05-08 19:11:01 -0300

    header update

--- src/react-app/routes/__root.tsx
+++ src/react-app/routes/__root.tsx
@@ -20,11 +20,11 @@ export const Route = createRootRoute({
                     items-center bg-neutral-700
                     mb-5 py-7 xl:px-40 md:px-17 sm:px-10 px-5
                     text-white'>
-                <div className="flex items-center mr-auto gap-7">
+                <div className="flex items-center mr-auto md:gap-7 gap-4">
                     <img className="h-15" src={logoTehuelches} alt="Logo" />
-                    <h1 className='font-rubik-dirt md:text-3xl sm:text-2xl text-xl'>Tehuelches SN</h1>
+                    <h1 className='font-caesar-dressing md:text-4xl text-3xl'>Tehuelches SN</h1>
                 </div>
-                <div className="mr-0 ml-auto flex gap-3">
+                <div className="mr-0 ml-auto flex gap-3 sm:flex-row flex-col">
                     <Link to="/" className={navButtonsClasses}>
                         Inicio
                     </Link>
