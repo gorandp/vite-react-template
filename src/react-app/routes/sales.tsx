4fcf20abc8514d9d62be4fb630f7ca0ffbae3e77
Goran Prpic <gorandp@outlook.com>
2025-05-16 18:47:08 -0300

    sales usage of header and footer

--- src/react-app/routes/sales.tsx
+++ src/react-app/routes/sales.tsx
@@ -1,9 +1,19 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { Header } from '@/components/Header'
+import { Footer } from '@/components/Footer'
+
 
 export const Route = createFileRoute('/sales')({
   component: RouteComponent,
 })
 
+
 function RouteComponent() {
-  return <div>Hello "/sales"!</div>
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <h3>Hello "/sales"!</h3>
+      <Footer />
+    </div>
+  )
 }
