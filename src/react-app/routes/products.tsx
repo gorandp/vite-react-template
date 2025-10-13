65fc74c53da76d3904739eb97c0567a51094841c
Goran Prpic <gorandp@outlook.com>
2025-10-12 21:36:15 -0300

    remove comments

--- src/react-app/routes/products.tsx
+++ src/react-app/routes/products.tsx
@@ -170,62 +170,6 @@ function RouteComponent() {
             deleteProduct={deleteProduct}
             setProductToUpdate={setProductToUpdate} />
         </section>
-        {/* 
-        <div className="card">
-          <button
-            onClick={() => setCount((count) => count + 1)}
-            aria-label="increment"
-          >
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
-        </div>
-        <div className="card">
-          <button
-            onClick={() => {
-              fetch("/api/products/initTable", {
-                method: "POST"
-              })
-                .then((res) => res.json() as Promise<{ name: string }>)
-                .then((data) => setName(data.name));
-            }}
-            aria-label="get name"
-          >
-            Create table products {name}
-          </button>
-          <button
-            onClick={() => {
-              fetch("/api/users", {
-                method: "POST",
-                headers: {
-                  "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ name: "gogos", password: "1234", roles: ["salesman"] })
-              })
-                .then((res) => res.json() as Promise<{ name: string }>)
-                .then((data) => setName(data.name));
-            }}
-            aria-label="get name"
-          >
-            Add user {name}
-          </button>
-          <button
-            onClick={() => {
-              fetch("/api/products")
-                .then((res) => res.json() as Promise<{ name: string }>)
-                .then((data) => setName(data.name));
-            }}
-            aria-label="get name"
-          >
-            Get products {name}
-          </button>
-          <p>
-            Edit <code>worker/index.ts</code> to change the name
-          </p>
-        </div>
-        <p className="read-the-docs text-3xl font-bold underline">Click on the logos to learn more</p> */}
       </div>
     </div>
   )
